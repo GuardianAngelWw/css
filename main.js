b1b2d092ef4caeaa6f8fdb01053903fa3573145f
GuardianAngelWw <137325971+GuardianAngelWw@users.noreply.github.com>
2023-08-21 21:15:55 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// Add your JavaScript code here
+// You can use this file to handle interactivity and dynamic content
+
+// Example: Add event listener to the group links
+const groupLinks = document.querySelectorAll('.group-links a');
+
+groupLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    // Add logic to handle link click event
+  });
+});
